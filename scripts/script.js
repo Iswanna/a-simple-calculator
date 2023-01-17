58e00b2d9d30f491f444fd7411e9d918bdf69944
iswat <ibeetek@gmail.com>
2023-01-17 10:38:23 +0000

    Add id attribute to all the button elements

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+//Catch DOM
+
+let outputScreen = document.getElementById("output-screen");
+let inputScreen = document.getElementById("input-screen");
+let button1 = document.querySelector(".grey")
